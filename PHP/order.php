8bc653a05746ad3cd624c91111e1b788e4ffb317
Moscolape <chunkums088@gmail.com>
2023-04-22 11:51:47 +0100

    push 4 folders for this project

---
+++ PHP/order.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="../CSS/order.css">
+    <link rel="shortcut icon" href="../images/game.jpg" type="image/x-icon">
+    <link href="https://fonts.googleapis.com/css2?family=Montez&family=Poppins&family=Tilt+Prism&display=swap" rel="stylesheet">
+</head>
+<body>
+    <!-- nav section -->
+    <nav>
+        <a href="home.html"><img src="../images/weblogo.jpg" alt="go-to-home_logo"></a>
+        <ul>
+            <li><a href="../PHP/products.php"> Products</a></li>
+            <li><a href="../PHP/search.php">Search</a></li>
+            <li><a href="../PHP/order.php">Order</a></li>
+            <li><a href="./about-us.html">About Us</a></li>
+            <li><a href="./contact-us.html">Contact Us</a></li>
+            <li><a href="./useful-links.html">Useful Links</a></li>
+        </ul>
+    </nav>
+
+
+    <style>
+        <?php include '../CSS/order.css' ?>
+    </style>
+</body>
+</html>
